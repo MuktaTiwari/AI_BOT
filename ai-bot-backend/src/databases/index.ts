150f2603a69737bab723fb741beb0953c74cc228
MuktaTiwari <72435930+MuktaTiwari@users.noreply.github.com>
2025-07-04 10:52:02 +0530

    Delete ai-bot-backend directory

--- ai-bot-backend/src/databases/index.ts
+++
@@ -1,9 +0,0 @@
-import { DB_HOST, DB_PORT, DB_DATABASE ,DB_USER ,DB_PASSWORD} from '@config';
-
-export const dbConfig = {
-  host: DB_HOST,
-  port: Number(DB_PORT),
-  database: DB_DATABASE,
-  user: DB_USER,
-  password: DB_PASSWORD,
-};
--- ai-bot-backend/src/interfaces/users.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  id: string;
-  userName: string;
-  userType: string;
-  email: string;
-  password: string;
-  createdAt: Date;
-  updatedAt : Date;
-}
